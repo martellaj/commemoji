559114b0c6c0d30f7abd15acf0ae5494750bf22e
Joe Martella <martellaj@live.com>
2015-12-31 15:21:57 -0800

    Added an error message for empty args. :rotating_light:

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+#! /usr/bin/env node
+
 var program = require('commander');
 var exec = require('child_process').exec;
+var chalk = require('chalk');
 
 program
   .version('1.0.0')
@@ -17,4 +20,6 @@ if (program.M) {
   exec('git commit -m "' + program.args[0] + '"', function (error, stdout, stderr) {
     console.log(stdout); 
   });
+} else {
+  console.log(chalk.red('You have to specify a commit message using the -m flag.'));
 }
